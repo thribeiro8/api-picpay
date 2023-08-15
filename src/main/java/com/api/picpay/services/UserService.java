ced75a8f90e87eb562f0546d43c6cb4c42c07966
Thomas Ribeiro <thomas18rs@gmail.com>
2023-08-15 17:50:25 -0300

    feat: create services

---
+++ src/main/java/com/api/picpay/services/UserService.java
@@ -0,0 +1,48 @@
+package com.api.picpay.services;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.picpay.domain.user.User;
+import com.api.picpay.domain.user.UserType;
+import com.api.picpay.dtos.UserDTO;
+import com.api.picpay.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário não está autorizado a realizar transação");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
+    }
+
+}
